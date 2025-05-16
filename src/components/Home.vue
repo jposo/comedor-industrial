8e2326e1894d557fa98f123c1acaa12bacf11c36
Juan <jposorniot@gmail.com>
2025-05-15 23:08:46 -0700

    First lambda

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,47 @@
 <template>
   <v-container> 
     <h2>Consumos del Día</h2>
-    <v-data-table-server>
-      v-model:item-per-page="5"
+    <v-data-table-server
+      v-model:item-per-page="itemsPerPage"
       :headers="headers"
       :items="items"
-    </v-data-table-server>
+      :loading="loading"
+      @update:options="loadConsumptions"
+    ></v-data-table-server>
   </v-container>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+
+const itemsPerPage = ref(10);
+const loading = ref(true);
+const headers = ref([
+  {title: 'id', key: 'id'},
+]);
+const items = ref([]);
+
+const loadConsumptions = async () => {
+  loading.value = true;
+  const response = await fetch('https://iqkwvr8wsh.execute-api.us-east-1.amazonaws.com/v1/ConsumosDelDia', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  const json = await response.json();
+  const body = JSON.parse(json.body);
+  console.log(body)
+  items.value = body.results;
+  loading.value = false;
+}
+
+// const response = await fetch('https://iqkwvr8wsh.execute-api.us-east-1.amazonaws.com/v1/ConsumosDelDia', {
+//   method: 'GET',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+// const items = await response.json();
+// console.log(items);
 </script>
